d93bab4aba0f2b01dcec51b6ab52111c10acd3b5
Hlynur Hansen <hlynurhansen@netscream.tolvur.net>
2016-11-06 19:16:04 +0000

    debugging enabled aftur

--- src/libs/debugging.h
+++ src/libs/debugging.h
@@ -12,7 +12,7 @@
  * Change this value to 1 (or something other than 0) to get debug function to work for tftp server 
  *
  */
-#define debug 0
+#define debug 1
 
 /* 
  * Function to print timestamp to output
