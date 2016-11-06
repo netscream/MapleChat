5e59b0238aa12ddf387d03825f37dd6b10a732ae
Theeep <tehkhaoz@gmail.com>
2016-11-06 19:12:37 +0000

    merge completed

--- src/libs/iterators.h
+++ src/libs/iterators.h
@@ -2,6 +2,7 @@
 #define ITERATORS_H
 #include "server.h"
 gboolean iter_connections(gpointer key, gpointer value, gpointer data);
+gboolean iter_live_connections(gpointer key, gpointer value, gpointer data);
 gboolean iter_add_to_fd_set(gpointer key, gpointer value, gpointer data);
 gboolean iter_users(gpointer key, gpointer value, gpointer data);
 gboolean iter_rooms(gpointer key, gpointer value, gpointer data);
